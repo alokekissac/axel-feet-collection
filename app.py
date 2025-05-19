7e2e1bfa80b298134e3abf44a3d86e05e819bab2
alokekissac <alokekissac@gmail.com>
2025-05-19 21:23:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 def index():
     images = []
     for filename in os.listdir(app.config['UPLOAD_FOLDER']):
-        if filename.endswith(('.png', '.jpg', '.jpeg', '.gif')) and filename not in ['axel-feet-drawing.png', 'leaf-bg.jpg']:
+        if filename.endswith(('.png', '.jpg', '.jpeg', '.gif')) and filename not in ['sonss.png', 'sunu.avif']:
             parts = filename.split('__', 1)
             name = parts[0] if len(parts) == 2 else 'Anonymous'
             images.append((filename, name))
